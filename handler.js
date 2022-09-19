30872ae22786acd126b18a3a9be791e7483f6677
mmanna3 <mmmannna3@gmail.com>
2022-09-19 10:16:44 -0300

    Modifica paths de cada endpoint y corrige mayúscula en import

--- handler.js
+++ handler.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const SecretsManager = require('./SecretsManager.js');
+const SecretsManager = require('./secretsManager.js');
 
 module.exports.hello = async (event) => {
   return {
